b02498d6d9a338a67c279a1935ae763fc3b76c27
unknown <sakibtaohid9@gmail.com>
2024-11-11 21:46:44 +0600

    solved no 5

---
+++ Leetcode/Easy-difficulty/find-index.js
@@ -0,0 +1,20 @@
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+// Example 1:
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+// Example 2:
+
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
+function findIndex(str,needle){
+    let index = str.indexOf(needle)
+    return index!==-1?index:-1
+    }
+console.log(findIndex('leetcode','leetoo'));
