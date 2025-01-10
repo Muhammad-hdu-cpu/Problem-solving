f08f9d7aa1f198a093a9c83b86ae477a1f2c35ec
unknown <sakibtaohid9@gmail.com>
2025-01-10 11:59:09 +0600

    vowel-count done

---
+++ Begginer/vowel-count.js
@@ -0,0 +1,11 @@
+function vowelCount(str){
+    const vowels = 'aeiou'
+    let count = 0
+    for(let i=0;i<str.length;i++){
+    if(vowels.includes(str[i])){
+    count++
+    }
+    }
+    return count
+}
+console.log(vowelCount('how are u'));
